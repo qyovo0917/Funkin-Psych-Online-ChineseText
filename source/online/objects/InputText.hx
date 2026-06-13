164f6c50c544b7dab5dd268370b4a67304c9967b
qyovo0917 <shengboxuan2010@outlook.com>
2026-06-14 01:06:24 +0800

    Update InputText.hx

--- source/online/objects/InputText.hx
+++ source/online/objects/InputText.hx
@@ -2,23 +2,25 @@ package online.objects;
 
 import online.gui.sidebar.SideUI;
 import flixel.addons.ui.FlxInputText;
+import openfl.text.IME;
+import openfl.text.IMEMode;
 
 class InputText extends FlxInputText {
     public function new(x:Float, y:Float, width:Float, onEnter:(text:String)->Void) {
         super(x, y, Std.int(width));
 
-        backgroundColor = FlxColor.TRANSPARENT;
-        fieldBorderColor = FlxColor.TRANSPARENT;
-        caretColor = FlxColor.WHITE;
+		backgroundColor = FlxColor.TRANSPARENT;
+		fieldBorderColor = FlxColor.TRANSPARENT;
+		caretColor = FlxColor.WHITE;
 
-        // 恢复控件默认的输入能力，不做额外修改
-        textField.selectable = true;
-        textField.editable = true;
-        textField.wordWrap = false;
+        #if desktop
+        textField.imeEnabled = true;
+        textField.imeMode = IMEMode.DEFAULT;
+        #end
 
         var prevText:String = '';
-        callback = (text, action) -> {
-            if (SideUI.instance != null && SideUI.instance.active) {
+		callback = (text, action) -> {
+			if (SideUI.instance != null && SideUI.instance.active) {
                 this.text = prevText;
                 return;
             }
@@ -26,8 +28,8 @@ class InputText extends FlxInputText {
             prevText = text;
 
             if (action == FlxInputText.ENTER_ACTION) {
-                hasFocus = false;
-                onEnter(text);
+				hasFocus = false;
+				onEnter(text);
             }
         };
     }
@@ -35,8 +37,15 @@ class InputText extends FlxInputText {
     override function update(elapsed) {
         super.update(elapsed);
 
-        // 移除之前的 FlxG.stage.focus 手动设置，避免干扰默认焦点逻辑
-        if (hasFocus && (FlxG.keys.justPressed.ESCAPE || (FlxG.mouse.justPressed && !FlxG.mouse.overlaps(this)))) {
+        #if desktop
+        if (hasFocus) {
+            IME.setCompositionEnabled(true);
+        } else {
+            IME.setCompositionEnabled(false);
+        }
+        #end
+
+		if (hasFocus && (FlxG.keys.justPressed.ESCAPE || (FlxG.mouse.justPressed && !FlxG.mouse.overlaps(this)))) {
             hasFocus = false;
         }
     }
