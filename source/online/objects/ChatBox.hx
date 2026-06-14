43f54296d1084fbf8033772697743ce46796931d
Lv233 <3506095220@qq.com>
2026-06-14 11:06:45 +0800

    Update ChatBox.hx
    Fix import lol

--- source/online/objects/ChatBox.hx
+++ source/online/objects/ChatBox.hx
@@ -5,7 +5,7 @@ import online.substates.RequestSubstate;
 import flixel.math.FlxRect;
 import openfl.events.KeyboardEvent;
 import lime.system.Clipboard;
-import online.object.InputText;
+import online.objects.InputText;
 
 class ChatBox extends FlxTypedSpriteGroup<FlxSprite> {
 	public static var instance:ChatBox;
