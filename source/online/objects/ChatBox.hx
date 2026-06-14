f5373e54950dbc1d4a64eb226a6cce0b6cf9bdb8
Lv233 <3506095220@qq.com>
2026-06-14 11:06:17 +0800

    Update ChatBox.hx

--- source/online/objects/ChatBox.hx
+++ source/online/objects/ChatBox.hx
@@ -5,6 +5,7 @@ import online.substates.RequestSubstate;
 import flixel.math.FlxRect;
 import openfl.events.KeyboardEvent;
 import lime.system.Clipboard;
+import online.object.InputText;
 
 class ChatBox extends FlxTypedSpriteGroup<FlxSprite> {
 	public static var instance:ChatBox;
